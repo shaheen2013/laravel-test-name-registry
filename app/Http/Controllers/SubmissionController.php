0205e76cd642c9acab478120770b5e343d00fad2
Noiem <coredeveloper.2013@gmail.com>
2024-02-23 22:40:09 +0600

    handled post request in controller

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SubmissionPostRequest;
+use App\Jobs\SaveSubmission;
+use App\Models\Submission;
 use Illuminate\Http\Request;
 
 class SubmissionController extends Controller
 {
-    public function post(SubmissionPostRequest $request) {
-        dd($request);
+    public function post(SubmissionPostRequest $request)
+    {
+        $submission = new Submission();
+        $submission->name = $request->name;
+        $submission->email = $request->email;
+        $submission->message = $request->message;
+
+        SaveSubmission::dispatch($submission);
+
+        return response()->json(['message' => 'Submission received'], 200);
     }
 }
