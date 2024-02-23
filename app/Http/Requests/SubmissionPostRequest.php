79e45ddcdf94b09b139e1dd7473e3aa98b3711be
Noiem <coredeveloper.2013@gmail.com>
2024-02-23 22:23:37 +0600

    api request validation rule added

--- app/Http/Requests/SubmissionPostRequest.php
+++ app/Http/Requests/SubmissionPostRequest.php
@@ -11,7 +11,7 @@ class SubmissionPostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class SubmissionPostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
         ];
     }
 }
